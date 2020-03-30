3abc65ac02671bc7c4a03c2ca93d4acaf81dd9a2
letitride <ichikawa.fumiya@gmail.com>
2020-03-30 10:46:28 +0900

    laravel セットアップをENTRYPOINTで定義 ENTRYPOINTはマウント実施後に実行される為、コンテナ内での成果物をホストに反映できる

--- app/cmd/api_login.php
+++
@@ -1,5 +0,0 @@
-<?php
-require ('../vendor/autoload.php');
-$client = new Everyman\Neo4j\Client("neo4j", 7474);
-$client->getTransport()->setAuth("neo4j", "testtest");
-
--- app/cmd/label.php
+++
@@ -1,9 +0,0 @@
-<?php
-require('api_login.php');
-$label = $client->makeLabel('Movie');
-$nodes = $label->getNodes();
-
-foreach( $nodes as $node ){
-  print("Title : " . $node->getProperty("title") . "\n" );
-  print("Released : " . $node->getProperty("released") . "\n\n" );
-}
